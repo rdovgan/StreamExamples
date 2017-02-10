77a3ff21c517d8835dbd8514b650ec3b7586870b
Roman Dovgan <romapsih@gmail.com>
2017-02-10 11:20:01 +0200

    Add reusable example and info about types of stream's methods

---
+++ resources/streams.md
@@ -0,0 +1,3 @@
+1. **Intermediate** operations like `map`, `filter`, `sorted`, `limit`, `skip`, `concat`, `substream`, `distinct`, `peek`, etc producing another java.util.stream.Stream<T>.
+
+2. **Terminal** operations like `forEach`, `reduce`, `collect`, `sum`, `max`, `count`, `matchAny`, `findFirst`, `findAny`, etc producing an object that is not a stream.
---
+++ src/org/streams/example/ReuseExample.java
@@ -0,0 +1,34 @@
+package org.streams.example;
+
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+/**
+ * Created by rdovgan on 10.02.2017.
+ */
+public class ReuseExample {
+
+	public static void notReusableStream() {
+		Stream<String> stream =
+				Stream.of("d2", "a2", "b1", "b3", "c")
+						.filter(s -> s.startsWith("b"));
+
+		stream.anyMatch(s -> true);    // ok
+		stream.noneMatch(s -> true);   // exception
+	}
+
+	public static void reusableStream() {
+		Supplier<Stream<String>> streamSupplier =
+				() -> Stream.of("d2", "a2", "b1", "b3", "c")
+						.filter(s -> s.startsWith("a"));
+
+		streamSupplier.get().anyMatch(s -> true);   // ok
+		streamSupplier.get().noneMatch(s -> true);  // ok
+	}
+
+	public static void main(String[] args) {
+		//notReusableStream();
+		reusableStream();
+	}
+
+}
